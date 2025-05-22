96beba95a5a1e9e195f77547d28de183d8415447
quan.nguyen <quan.nguyen@torus.vn>
2025-05-22 17:38:07 +0700

    refactor: remove native modules

--- lib/typescript/src/modal/aysnc-storage-logger.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-declare const KeyValueTable: React.FC;
-export default KeyValueTable;
-//# sourceMappingURL=aysnc-storage-logger.d.ts.map
--- lib/typescript/src/modal/redux-logger.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-declare const ReduxLogger: React.FC;
-export default ReduxLogger;
-//# sourceMappingURL=redux-logger.d.ts.map
